65b499ded5b58e9de390958f3b4b127c23d02fda
briantam23 <briantam23@gmail.com>
2019-07-08 21:49:55 -0400

    Updated Media Queries

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -11,25 +11,24 @@ class Map extends Component {
   }
 
   componentDidMount = () => {
-    const { user } = this.state;
 
     window.initMap = this.initMap;
     loadScript(`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_API_KEY}&libraries=places&callback=initMap`)
 
     return axios.get('/api/users')
       .then(res => res.data)
-      .then(users => {
+      /* .then(users => {
         console.log(users)
         this.setState({ user: users[0] })
-      })
+      }) */
       /* .then(() => {
         //Testing alert by city or Lat/Long!
         //Can also initiate by new Date() function
         setInterval(() => {
-          return axios.get(`http://api.waqi.info/feed/${user.cities[0].name}/?token=${process.env.AIR_QUALITY_INDEX_KEY}`)
+          return axios.get(`http://api.waqi.info/feed/${this.state.user.cities[0].name}/?token=${process.env.AIR_QUALITY_INDEX_KEY}`)
             .then(res => res.data.data.aqi)
             .then(aqi => {
-              if (aqi >= user.cities[0].aqiThreshold) {
+              if(aqi >= this.state.user.cities[0].aqiThreshold) {
                 axios.post('/api/messages', {
                   "to": "5166109915",
                   "body": "Air Quality Index > 0"

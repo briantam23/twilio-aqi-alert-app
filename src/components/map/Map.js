af1a3124f4e749426ad94058a2254b4d5ccf3fd5
briantam23 <briantam23@gmail.com>
2019-07-13 19:16:07 -0400

    Cleaned up Google Maps functions

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment, createRef } from 'react';
 import axios from 'axios';
-import { loadScript } from '../../util';
+import { loadScript, _initMap } from '../../util';
 import style from './map.less';
 
 
@@ -12,8 +12,9 @@ class Map extends Component {
 
   componentDidMount = () => {
 
-    window.initMap = this.initMap;
-    loadScript(`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_API_KEY}&libraries=places&callback=initMap`)
+    const initMap = () => _initMap(this, document);
+    window.initMap = initMap;
+    loadScript(`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_API_KEY}&libraries=places&callback=initMap`);
 
     return axios.get('/api/users')
       .then(res => res.data)
@@ -23,154 +24,28 @@ class Map extends Component {
       })
       .then(() => {
         //Testing alert by city or Lat/Long!
-        //Can also initiate by new Date() function
-        //setInterval(() => {}, 10000)
-
 
         setInterval(() => {
           const currentDate = new Date();
           const currentHour = currentDate.getHours();
-          const currentMin = currentDate.getMinutes();
-          const currentSec = currentDate.getSeconds();
-          
+
           console.log(currentHour);
-        
-          if(currentHour === 18 || currentHour === 8) {
+
+          if (currentHour === 18 || currentHour === 8) {
             console.log('if')
             return axios.get(`https://api.waqi.info/feed/${this.state.user.cities[0].name}/?token=${process.env.AIR_QUALITY_INDEX_KEY}`)
-                .then(res => res.data.data.aqi)
-                .then(aqi => {
-                  if(aqi >= this.state.user.cities[0].aqiThreshold) {
-                    axios.post('/api/messages', {
-                      "to": "5166109915",
-                      "body": "Air Quality Index > 0"
-                    })
-                  }
-                })
+              .then(res => res.data.data.aqi)
+              .then(aqi => {
+                if (aqi >= this.state.user.cities[0].aqiThreshold) {
+                  axios.post('/api/messages', {
+                    "to": "5166109915",
+                    "body": "Air Quality Index > 0"
+                  })
+                }
+              })
           }
         }, 3600000)
       })
-  }
-
-  initMap = () => {
-    let marker;
-
-    // Map
-    const map = new google.maps.Map(this.refs.map, {
-      center: new google.maps.LatLng(40.7048, -73.6501),
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      zoom: 10
-    })
-    
-    // Search box
-    const input = document.getElementById('autocomplete');
-    const searchBox = new google.maps.places.SearchBox(input);
-    map.controls[google.maps.ControlPosition.TOP_RIGHT].push(input);
-
-    // Autocomplete
-    const autocomplete = new google.maps.places.Autocomplete(input, { types: ['geocode'] });
-    autocomplete.addListener('place_changed', onPlaceChanged);
-
-    // On Place Change
-    const places = new google.maps.places.PlacesService(map);
-
-    function onPlaceChanged() {
-      const place = autocomplete.getPlace();
-      const position = place.geometry.location;
-      if(place.geometry) {
-        map.panTo(position);
-        map.setZoom(10);
-        search();
-        // Marker
-        marker = new google.maps.Marker({ position });
-        setTimeout(dropMarker(), 1);
-      }
-      else document.getElementById('autocomplete').placeholder = 'Enter a city';
-    }
-
-    // Legend
-    map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(document.getElementById('legend-computer'));
-    map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(document.getElementById('legend-mobile'));
-
-    // Air Quality
-    const waqiMapOverlay = new google.maps.ImageMapType({
-      getTileUrl: (coord, zoom) => 'https://tiles.waqi.info/tiles/usepa-aqi/' + zoom + '/' + coord.x + '/' + coord.y + `.png?token=${process.env.AIR_QUALITY_INDEX_KEY}`,
-      name: 'Air Quality',
-    })
-    map.overlayMapTypes.insertAt(0, waqiMapOverlay);
-
-    // HTML5 geolocation.
-    const infoWindow = new google.maps.InfoWindow;
-    if(navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-          };
-
-          infoWindow.setPosition(pos);
-          infoWindow.setContent('Location found!');
-          infoWindow.open(map);
-          map.setCenter(pos);
-        },
-        () => handleLocationError(true, infoWindow, map.getCenter())
-      )
-    }
-    else handleLocationError(false, infoWindow, map.getCenter());
-
-    const handleLocationError = (browserHasGeolocation, infoWindow, pos) => {
-      infoWindow.setPosition(pos);
-      infoWindow.setContent(browserHasGeolocation ?
-        'Error: The Geolocation service failed.' :
-        'Error: Your browser doesn\'t support geolocation.');
-      infoWindow.open(map);
-    }
-
-    // Set Marker
-    const search = () => {
-      const _search = { bounds: map.getBounds() };
-      clearMarker();
-      places.nearbySearch(_search, (place, status) => {
-        if(status === google.maps.places.PlacesServiceStatus.OK && place.geometry) {
-          marker = new google.maps.Marker({ position });
-          dropMarker();
-        }
-      })
-    }
-    const toggleBounce = () => {
-      if(marker.getAnimation() !== null) marker.setAnimation(null);
-      else marker.setAnimation(google.maps.Animation.BOUNCE);
-    }
-    const clearMarker = () => {
-      if(marker) marker.setMap(null);
-      if(infoWindow) infoWindow.close();
-      marker = null;
-    }
-    const dropMarker = () => {
-      return () => {
-        marker.setMap(map);
-        marker.setAnimation(google.maps.Animation.DROP);
-        marker.addListener('click', toggleBounce);
-      }
-    }
-
-    // Bias results towards geolocation
-    const geolocate = () => {
-      if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((position) => {
-          const geolocation = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-          }
-          const circle = new google.maps.Circle({
-            center: geolocation,
-            radius: position.coords.accuracy
-          })
-          autocomplete.setBounds(circle.getBounds());
-        })
-      }
-    }
   }
 
   render() {
@@ -179,17 +54,17 @@ class Map extends Component {
       <Fragment>
         <div ref="map" className={style.mapContainer} />
         <input
-            id='autocomplete'
-            className={style.autocomplete}
-            placeholder='Check a location'
-            spellCheck='false'
-            autoFocus
+          id='autocomplete'
+          className={style.autocomplete}
+          placeholder='Check a location'
+          spellCheck='false'
+          autoFocus
         />
         <div id='legend-computer' className={style.legendComputer}>
-          <img src='/public/img/aqi_legend_computer.png' alt='AQI Legend Computer'/>
+          <img src='/public/img/aqi_legend_computer.png' alt='AQI Legend Computer' />
         </div>
         <div id='legend-mobile' className={style.legendMobile}>
-            <img src='/public/img/aqi_legend_mobile.png' alt='AQI Legend Mobile'/>
+          <img src='/public/img/aqi_legend_mobile.png' alt='AQI Legend Mobile' />
         </div>
       </Fragment>
     )
--- src/util.js
+++ src/util.js
@@ -1,7 +1,136 @@
 export const loadScript = src => {
-    var ref = window.document.getElementsByTagName("script")[0];
-    var script = window.document.createElement("script");
-    script.src = src;
-    script.async = true;
-    ref.parentNode.insertBefore(script, ref);
+  const ref = window.document.getElementsByTagName("script")[0];
+  const script = window.document.createElement("script");
+  script.src = src;
+  script.async = true;
+  ref.parentNode.insertBefore(script, ref);
+}
+
+
+// Map
+const mapFunc = _this => (
+  new google.maps.Map(_this.refs.map, {
+    center: new google.maps.LatLng(40.7048, -73.6501),
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    zoom: 10
+  })
+)
+
+
+// Legend
+/* const legendFunc = () => {
+    map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(document.getElementById('legend-computer'));
+    map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(document.getElementById('legend-mobile'));
+} */
+
+
+// AQI Map Overlay
+const waqiMapOverlayFunc = () => (
+  new google.maps.ImageMapType({
+    getTileUrl: (coord, zoom) => 'https://tiles.waqi.info/tiles/usepa-aqi/' + zoom + '/' + coord.x + '/' + coord.y + `.png?token=${process.env.AIR_QUALITY_INDEX_KEY}`,
+    name: 'Air Quality',
+  })
+)
+
+
+const geolocation = (map, infoWindow) => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('Location found!');
+        infoWindow.open(map);
+        map.setCenter(pos);
+      },
+      () => handleLocationError(true, infoWindow, map.getCenter(), map)
+    )
+  }
+  else handleLocationError(false, infoWindow, map.getCenter(), map);
+}
+
+const handleLocationError = (browserHasGeolocation, infoWindow, pos, map) => {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+}
+
+
+// Set Marker
+const search = (map, marker, infoWindow, places) => {
+  const _search = { bounds: map.getBounds() };
+  clearMarker(marker, infoWindow);
+  places.nearbySearch(_search, (place, status) => {
+    if (status === google.maps.places.PlacesServiceStatus.OK && place.geometry) {
+      marker = new google.maps.Marker({ position });
+      dropMarker(marker, map, toggleBounce);
+    }
+  })
+}
+
+
+const clearMarker = (marker, infoWindow) => {
+  if (marker) marker.setMap(null);
+  if (infoWindow) infoWindow.close();
+  marker = null;
+}
+
+const toggleBounce = marker => () => {
+  if (marker.getAnimation() !== null) marker.setAnimation(null);
+  else marker.setAnimation(google.maps.Animation.BOUNCE);
+}
+
+const dropMarker = (marker, map, toggleBounce) => () => {
+  marker.setMap(map);
+  marker.setAnimation(google.maps.Animation.DROP);
+  marker.addListener('click', toggleBounce(marker));
+}
+
+
+export const _initMap = _this => {
+  let marker;
+
+  const map = mapFunc(_this);
+
+  // Search box
+  const input = document.getElementById('autocomplete');
+  const searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_RIGHT].push(input);
+
+  // Autocomplete
+  const autocomplete = new google.maps.places.Autocomplete(input, { types: ['geocode'] });
+  autocomplete.addListener('place_changed', onPlaceChanged);
+
+  // On Place Change
+  const places = new google.maps.places.PlacesService(map);
+
+  function onPlaceChanged() {
+    const place = autocomplete.getPlace();
+    const position = place.geometry.location;
+    if (place.geometry) {
+      map.panTo(position);
+      map.setZoom(10);
+      search(map, marker, infoWindow, places);
+
+      // Marker
+      marker = new google.maps.Marker({ position });
+      setTimeout(dropMarker(marker, map, toggleBounce), 1);
+    }
+    else document.getElementById('autocomplete').placeholder = 'Enter a city';
+  }
+
+  // Air Quality
+  const waqiMapOverlay = waqiMapOverlayFunc();
+  map.overlayMapTypes.insertAt(0, waqiMapOverlay);
+
+  // HTML5 geolocation.
+  const infoWindow = new google.maps.InfoWindow;
+  geolocation(map, infoWindow);
+
 }

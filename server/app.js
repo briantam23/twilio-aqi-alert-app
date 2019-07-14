4d1fdaeae820e811a64411d8dfdb8cb51dc6243a
briantam23 <briantam23@gmail.com>
2019-07-14 01:42:18 -0400

    Created auth routes

--- server/app.js
+++ server/app.js
@@ -23,6 +23,26 @@ app.use('/public', express.static(path.join(__dirname, '../public')));
 // Sends our index.html (the "single page" of our SPA)
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
 
+// Auth middleware
+app.use((req, res, next) => {
+    const token = req.headers.authorization;
+    if(!token) return next();
+    
+    let id;
+    try {
+        id = jwt.decode(token, process.env.JWT_SECRET).id;
+    }
+    catch(ex) {
+        return next({ status: 401 });
+    }
+    User.findByPk(id)
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(next);
+})
+
 // Routes that will be accessed via AJAX that are prepended 
 // with /api so that they are isolated from our GET /* wildcard.
 app.use('/api/messages', require('./routes/messages'));

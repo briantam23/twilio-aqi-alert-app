4b5ddd183dfc8326d4e09f1080cb299044524edd
briantam23 <briantam23@gmail.com>
2019-07-27 23:44:07 -0400

    Finished Redux tests

--- test/store.test.js
+++ test/store.test.js
@@ -25,4 +25,52 @@ describe('The `User` Redux store', () => {
             })
         })
     })
+
+    describe('usersReducer', () => {
+
+        let testingStore;
+        beforeEach('Create testing store from reducer', () => {
+            testingStore = createStore(usersReducer);
+        })
+
+        it('has an initial state as an empty array', () => {
+
+            const currentStoreState = testingStore.getState();
+
+            expect(currentStoreState).to.be.deep.equal([]);
+        })
+
+        it('reduces on CREATE_USER (without mutating previous state)', () => {
+
+            const prevState = testingStore.getState();
+
+            testingStore.dispatch({
+                type: CREATE_USER,
+                user
+            })
+
+            const newState = testingStore.getState();
+
+            expect(newState.length).to.be.equal(prevState.length + 1);
+            expect(newState[newState.length - 1]).to.be.deep.equal(user);
+            expect(newState).to.not.be.equal(prevState);
+        })
+
+        it('handles unrecognized actions & returns the previous state', () => {
+
+            const prevState = testingStore.getState();
+
+            testingStore.dispatch({
+                type: 'NOT_A_THING'
+            })
+
+            const newState = testingStore.getState();
+
+            //these should be the same array in memory AND have equivalent key-value pairs
+            expect(prevState).to.be.an('array');
+            expect(newState).to.be.an('array');
+            expect(newState).to.be.equal(prevState);
+            expect(newState).to.be.deep.equal(prevState);
+        })
+    })
 })

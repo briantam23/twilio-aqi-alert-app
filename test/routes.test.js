6af948b34128518a90a86349abfe1701e69d1fcf
briantam23 <briantam23@gmail.com>
2019-08-04 08:50:55 -0400

    Finished testing for GET routes

--- test/routes.test.js
+++ test/routes.test.js
@@ -85,6 +85,69 @@ describe('The Express Server', () => {
                 expect(res.body).to.be.an.instanceOf(Array);
                 expect(res.body[0].password).to.equal('Briantam23@');
             })
+
+            it('returns another User if there is one in the DB', async() => {
+
+                await User.create({ 
+                    username: 'Brian', 
+                    password: 'Briantam23@', 
+                    phoneNumber: '5166109915' 
+                })
+                await User.create({ 
+                    username: 'Mike',
+                    password: 'Mike12#', 
+                    phoneNumber: '5164337766' 
+                })
+    
+                const res = await agent
+                    .get('/api/users')
+                    .expect(200)
+    
+                expect(res.body).to.be.an.instanceOf(Array);
+                expect(res.body[0].username).to.equal('Brian');
+                expect(res.body[1].username).to.equal('Mike');
+            })
+        })
+
+        describe('GET /api/users/:userId', () => {
+
+            let testUser; 
+            beforeEach(async () => {
+                const creatingUsers = [
+                    { 
+                        username: 'Brian', 
+                        password: 'Briantam23@', 
+                        phoneNumber: '5166109915' 
+                    },
+                    { 
+                        username: 'Mike',
+                        password: 'Mike12#', 
+                        phoneNumber: '5164337766' 
+                    }
+                ].map(data => User.create(data));
+                
+                const createdUsers = await Promise.all(creatingUsers);
+                testUser = createdUsers[1];
+            })
+
+            it('returns the JSON of the User based on the id', async () => {
+                
+                const res = await agent
+                    .get('/api/users/' + testUser.id)
+                    .expect(200);
+    
+                if(typeof res.body === 'string') {
+                    res.body = JSON.parse(res.body);
+                }
+                expect(res.body.username).to.equal('Mike');
+            })
+
+            it('returns a 404 error if the ID is not correct', () => {
+                
+                return agent
+                    .get('/api/users/a91bb9d6-53dc-49f3-8358-1334e3941bd7')
+                    .expect(404);
+            })
         })
     })
 })
